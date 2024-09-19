e905f92ba78e9bf859875e322e16e5395daf10e7
Veronica <veromartina8@gmail.com>
2024-09-18 22:40:31 -0300

    primeros pasos para traer la api en el archivo js

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
+
+// public key
+//99d7c07d74c026e85a23c1dbdb1454bd
+
+//private key
+//2c70912eacc85117c854d1cd8bf4d0d869fb631d
+
+//hash=  b1da2d2133efcaee66a056c03b5b33ce
+
+//ts = "marvel"
+
+
+const urlApi = "http://gateway.marvel.com";
+const urlPersonajes = "/v1/public/characters";
+const publicKey = "99d7c07d74c026e85a23c1dbdb1454bd";
+const ts = "marvel";
+const hash = "b1da2d2133efcaee66a056c03b5b33ce";
+const paramAutenticacion = `?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+
+fetch(urlApi + urlPersonajes + paramAutenticacion, {
+  method: "GET",
+  headers: {
+    /* Authorization: `${publicKey}`, */
+    "Content-type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
+
+
+
+
 /*
-Your public key
-99d7c07d74c026e85a23c1dbdb1454bd
-
-Your private key
-2c70912eacc85117c854d1cd8bf4d0d869fb631d
-
-Read more about how to use your keys to sign requests.
-
-Your rate limit:
- 
-3000
-calls/day Number of calls your application can make per day.
-
-hash=  
-
-*/
-/*
-const publicKey = '';
-const ts = 3000;
-const hash = 'c4d792be383164231dbf43283ce1ef7b';
-
 const limit = 20;
 let offset = 0;
 let totalResults = 0;
