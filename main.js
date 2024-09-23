2444bd4c9fd24f48491e111eedddff3f89f3616c
Veronica <veromartina8@gmail.com>
2024-09-23 00:18:15 -0300

    se agrega archivo normalice. se logra traer comics y se acomodan algunos estilos para mobile del contendor comics.
    se acomodan carpetas
    
    Actividad

--- main.js
+++ main.js
@@ -1,56 +1,65 @@
-
-const urlBase = "http://gateway.marvel.com/v1/public" //¿PUEDO USARLA PARA TRAER TODO (COMIC Y PERSONAJES)
-
-const urlApi = "http://gateway.marvel.com";
-const urlPersonajes = "/v1/public/characters";
 const publicKey = "99d7c07d74c026e85a23c1dbdb1454bd";
 const ts = "marvel";
 const hash = "b1da2d2133efcaee66a056c03b5b33ce";
+
+const urlApi = "http://gateway.marvel.com";
+const urlPersonajes = "/v1/public/characters";
+const urlComics = "/v1/public/comics";
+
 const paramAutenticacion = `?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
 
 const contenedorResultados = document.getElementById("contenedor-resultados"); // contenedor div del html
 
-function crearTarjetas(tarjetas) {
-    tarjetas.forEach(tarj => {
-        const nuevaTarjeta = document.createElement('div');
-        nuevaTarjeta.className = 'card';
-
-        const nuevaImagen = document.createElement('img');
-        nuevaImagen.className = 'imgCard';
-        
-        const imageUrl = `${tarj.thumbnail.path}.${tarj.thumbnail.extension}`; 
-        nuevaImagen.src = imageUrl;
-
-        const descripcion = document.createElement('h3');
-        descripcion.className ='descripCard';
-        descripcion.textContent = tarj.name; 
-        descripcion.style.marginTop = '10px'; // Separar imagen del texto
-
-        nuevaTarjeta.appendChild(nuevaImagen);
-        nuevaTarjeta.appendChild(descripcion);
-        contenedorResultados.appendChild(nuevaTarjeta);
-        
-        //PARA MOSTRAR EL DETALLE DE CADA CARD
-        nuevaTarjeta.addEventListener('click', function() {  //funcion que debe mostarme en detalle : Se debe mostrar la información de un cómic (al clickear sobre el mismo)Portada-Título-Fecha de lanzamiento-Guionistas-Descripción-Personajesincluidos
-        //Se debe mostrar la información de un personaje (al clickear sobre el mismo:Imagen-Nombre-Descripción-Cómics en los que aparece.
-            alert(tarj.name);   // a modo de ej,
-        });
-    });
-}
-
 function mostrarTarjetas() {
-  fetch(urlApi + urlPersonajes + paramAutenticacion) 
+  fetch(urlApi +  urlComics + paramAutenticacion) 
   .then((response) => response.json())
 
   .then((data) => {
+    
+    const comics = data.data.results; // Accede a los 
     console.log(data);
-    const personajes = data.data.results; // Accede a los personajes
-    crearTarjetas(personajes);}) // Llama a la función para crear tarjeta
+    crearTarjetas(comics);}) // Llama a la función para crear tarjeta
 
   .catch((error) => console.error(error));
   }
 
 mostrarTarjetas();
+
+function crearTarjetas(tarjetas) {
+    tarjetas.forEach(tarj => {
+        const nuevaTarjetaComics = document.createElement('div');
+        nuevaTarjetaComics.className = 'cardComics';
+        nuevaTarjetaComics.setAttribute('tabindex', '0');;  //Hace que el div sea enfocable y su posición en el orden de tabulación depende de su lugar en el HTML.Mejora la accesibilidad y permite la interacción del usuario.
+        contenedorResultados.appendChild(nuevaTarjetaComics);
+
+        const conteImgComics = document.createElement("div");
+        conteImgComics.className = "contenedorImgComics";
+        nuevaTarjetaComics.appendChild(conteImgComics);
+
+        const nuevaImagenComics = document.createElement('img');
+        nuevaImagenComics.className = 'imgComics';
+        const imageUrlComics = `${tarj.thumbnail.path}.${tarj.thumbnail.extension}`; 
+        nuevaImagenComics.src = imageUrlComics;
+        conteImgComics.appendChild(nuevaImagenComics);
+
+        const tituloComics = document.createElement('h3');
+        tituloComics.className ='tituloComics';
+        tituloComics.textContent = `${tarj.title}`; 
+        tituloComics.style.marginTop = '10px'; // Separar imagen del texto
+        nuevaTarjetaComics.appendChild(tituloComics);
+        
+        
+        
+        
+        //PARA MOSTRAR EL DETALLE DE CADA CARD
+        //nuevaTarjeta.addEventListener('click', function() {  //funcion que debe mostarme en detalle : Se debe mostrar la información de un cómic (al clickear sobre el mismo)Portada-Título-Fecha de lanzamiento-Guionistas-Descripción-Personajesincluidos
+        //Se debe mostrar la información de un personaje (al clickear sobre el mismo:Imagen-Nombre-Descripción-Cómics en los que aparece.
+        //    alert(tarj.name);   // a modo de ej,
+        });
+      }
+
+
+
 
 /*  REALIZADO EN CLASE --------------------
 const urlApi = "http://gateway.marvel.com";
